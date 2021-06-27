ff7869cc26773882d7d094578cbca40444535160
Adnan Rahman Robi <55150977+Adnanrobi@users.noreply.github.com>
2021-06-27 20:13:35 +0600

    established database connection

--- Lab_2_180042108/index.js
+++ Lab_2_180042108/index.js
@@ -1,5 +1,18 @@
 const app = require('./app');
+const mongoose = require('mongoose');
 require('dotenv').config();
+
+mongoose.connect(process.env.MONGOURL, {
+    useNewUrlParser: true,
+    useCreateIndex : true,
+    useUnifiedTopology : true
+}).then(() => {
+    console.log('Database Connection established');
+}).catch(err => {
+    if (err) {
+        console.log('Database connection failed');
+    }
+})
 
 const PORT = process.env.PORT;
 
