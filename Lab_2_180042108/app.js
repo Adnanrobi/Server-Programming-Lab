b17825b478fda27ae8f059744d214e520d2494fe
Adnan Rahman Robi <55150977+Adnanrobi@users.noreply.github.com>
2021-06-27 13:10:09 +0600

    worked on controller file

--- Lab_2_180042108/app.js
+++ Lab_2_180042108/app.js
@@ -3,12 +3,12 @@ const cors = require('cors');
 const userRoutes = require('./routes/userRoutes.routes');
 
 const app = express();
+
 app.use(cors());
+app.use(userRoutes);
 
 app.get('/', (req, res) => {
     res.sendFile('index3.html', { root: './views'})
 });
-
-app.use(userRoutes);
 
 module.exports = app;
---
+++ Lab_2_180042108/controllers/userController.controller.js
@@ -0,0 +1,5 @@
+const getLoginPage = (req, res) => {
+    res.send('loginPage from controller');
+};
+
+module.exports = { getLoginPage };
