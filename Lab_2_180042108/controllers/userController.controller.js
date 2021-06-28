6ed85b1195cb70c563b4c4e40c91bb2a7e40c0b3
Adnan Rahman Robi <55150977+Adnanrobi@users.noreply.github.com>
2021-06-28 16:32:52 +0600

    can store data in database now

--- Lab_2_180042108/controllers/userController.controller.js
+++ Lab_2_180042108/controllers/userController.controller.js
@@ -10,16 +10,56 @@ const getRegisterPage = (req, res) => {
     res.sendFile('register.html', { root: './views/pages/examples' });
 };
 
-const postRegister = (req, res) => {
+const postRegister = async (req, res) => {
     const name = req.body.name;
     const email = req.body.email;
     const password = req.body.password;
     const retype = req.body.retype;
 
-    if (password.length < 6 || !name || !email) { return res.json({ message: 'Invalid Registration' }) }
-    
-    res.json({ message: 'registration successful' })
-        
+    try
+    {
+        const user = await User.findone({ email });
+        if (user)
+        {
+            alert("Already registered");
+            res.redirect("/login")
+        }
+        else if (password.length < 6)
+        {
+            alert("Password must be at least 6 characters!");
+            res.redirect("/register");
+        }
+        else if (password !== retype)
+        {
+            alert("Retype the password properly")
+            res.redirect("/register");
+        }
+        else if (!name || !email || !password || !retype)
+        {
+            alert("fields can't be empty")
+            res.redirect("/register");
+        }
+        else
+        {
+            const salt = await bcrypt.genSaltSync(8);
+            const passwordHash = await bcrypt.hash(password, salt);
+            const createUser = new User({
+                name,
+                email,
+                passwordHash
+            });
+            await createUser.save();
+            alert("Success!");
+            res.redirect('/login');
+        }
+    }
+
+    catch (error)
+    {
+        console.error(error);
+        alert('Something went wrong');
+        res.redirect('/register');
+    }    
 };
 
 module.exports = {
