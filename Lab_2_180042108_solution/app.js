c5e963c54b3789e8effe247874b1783c86d4ae68
Adnan Rahman Robi <55150977+Adnanrobi@users.noreply.github.com>
2021-07-01 12:41:05 +0600

    completed error handling for registration

--- Lab_2_180042108_solution/app.js
+++ Lab_2_180042108_solution/app.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const app = express();
+const session = require('express-session');
+const flash = require('connect-flash');
 
 //static resources:
 app.use(express.static("public"));
 
 //View Engine:
 app.set("View Engine", "ejs");
+
+//Session and Flash:
+app.use(
+    session({
+        secret: "secret",
+        resave: true,
+        saveUninitialized: true,
+    })
+);
+app.use(flash());
 
 //body_Parser :
 app.use(express.urlencoded({extended: false }));
--- Lab_2_180042108_solution/controllers/users.controller.js
+++ Lab_2_180042108_solution/controllers/users.controller.js
@@ -3,11 +3,13 @@ const getLogin = (req, res) => {
 }
 
 const postLogin = (req, res) => {
-
+    const { email, password } = req.body;
+    console.log(email);
+    console.log(password);
 }
 
 const getRegister = (req, res) => {
-    res.render('users/register-v2.ejs')
+    res.render('users/register-v2.ejs', { errors: req.flash('errors') });
 }
 
 const postRegister = (req, res) => {
@@ -16,7 +18,30 @@ const postRegister = (req, res) => {
     console.log(email);
     console.log(password);
     console.log(confirm_password);
-}
+
+    const errors = []
+    if (!name || !email || !password || !confirm_password) {
+        errors.push("All fields are required");
+    }
+    if (password.length < 6) {
+        errors.push("Passwords must be at least 6 characters");
+    }
+    if (password !== confirm_password) {
+        errors.push("Passwords do not match");
+    }
+    if (errors.length > 0) {
+        req.flash('errors', errors);
+        res.redirect("/users/register");
+    } else {
+        //Create a new user:
+        res.redirect("/users/login");
+    }
+    
+};
+
+//Data validation:
+
+
 
 module.exports = {
     getLogin,
